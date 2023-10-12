6f67d2b45701f442449dfbfb1f9c3f328b485eaf
Morgan Kaufman <Morgankaufman13@yahoo.com>
2023-10-12 19:54:27 -0400

    weekly challenge 1 finished

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -7,7 +7,7 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
+            if(c == )
         }
 
         public bool CountOfElementsIsEven(string[] vals)
